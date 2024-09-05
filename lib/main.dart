3d9a9b8845ace4624ca9fbb30ca5b9b2e3759325
daksh3003 <dakshpandey3003@gmail.com>
2024-09-06 01:47:52 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:agriplant/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
