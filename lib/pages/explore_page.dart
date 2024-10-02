7cb25a1eb414e84d6e38655dcaf73e0ec3c66ba1
aniket0880 <aabaravkar456@gmail.com>
2024-10-03 00:43:44 +0530

    minor changes

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:agriplant/data/products.dart';
 import 'package:agriplant/newpages/Contact_page.dart';
 import 'package:agriplant/widgets/product_card.dart';
-// Import the new contact page
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
@@ -11,118 +10,123 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 15),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Search here...",
-                      isDense: true,
-                      contentPadding: const EdgeInsets.all(12.0),
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(99),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search here...",
+                        isDense: true,
+                        contentPadding: const EdgeInsets.all(12.0),
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(99),
+                          ),
                         ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade300,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(99),
+                          ),
+                        ),
+                        prefixIcon: const Icon(IconlyLight.search),
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey.shade300,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: IconButton.filled(onPressed: () {}, icon: const Icon(IconlyLight.filter)),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: SizedBox(
+                height: 170,
+                child: Card(
+                  color: Colors.green.shade50,
+                  elevation: 0.1,
+                  shadowColor: Colors.green.shade50,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Free consultation",
+                                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                                  color: Colors.green.shade700,
+                                ),
+                              ),
+                              const Text("Get free support from our customer service"),
+                              FilledButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const ContactPage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text("Contact Us"),
+                              ),
+                            ],
+                          ),
                         ),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(99),
+                        Image.asset(
+                          'assets/contact_us.png',
+                          width: 140,
                         ),
-                      ),
-                      prefixIcon: const Icon(IconlyLight.search),
+                      ],
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 12),
-                  child: IconButton.filled(onPressed: () {}, icon: const Icon(IconlyLight.filter)),
-                ),
-              ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 25),
-            child: SizedBox(
-              height: 170,
-              child: Card(
-                color: Colors.green.shade50,
-                elevation: 0.1,
-                shadowColor: Colors.green.shade50,
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Flexible(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Free consultation",
-                              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                                color: Colors.green.shade700,
-                              ),
-                            ),
-                            const Text("Get free support from our customer service"),
-                            FilledButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const ContactPage(),
-                                  ),
-                                );
-                              },
-                              child: const Text("Contact Us"),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Image.asset(
-                        'assets/contact_us.png',
-                        width: 140,
-                      )
-                    ],
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Explore",
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("See all"),
+                  ),
+                ],
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Explore",
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("See all"),
-              ),
-            ],
-          ),
-          ListView.builder(
-            itemCount: products.length,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: ProductCard(product: products[index]),
-              );
-            },
-          )
-        ],
+            ListView.builder(
+              itemCount: products.length,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: ProductCard(product: products[index]),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
